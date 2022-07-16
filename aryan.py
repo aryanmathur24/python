15be6c80fb76fa692d1efe2d6aa3ef1f1fd91cc7
aryan <aryan@mail.com>
2022-07-16 13:37:26 +0530

    intial commit

---
+++ aryan.py
@@ -0,0 +1,51 @@
+# from platform import python_version
+
+
+# fname = 'Aryan'
+# lname = 'Mathur'
+# print(fname.count('a'))
+# lst = [1, 2, 3, 4, 5, 'hello', ['a' , 'b' , 'c']]
+# print(lst[0])
+# print(lst[-1])
+# print(lst [-2][0])
+# random_number_list = [10,30,60,40,20,100]
+# print(random_number_list)
+# print(random_number_list.sort())
+# s='aryan'
+# ls=list(s)
+# ls
+# a = [1, 2, 3, 4]
+# b = [5, 6, 7, 8]
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)
+# a.insert(1, "hello")
+# print (a)
+# a.insert(-1, "hgjh")
+# python_version
+
+# a = [1,2,3,4, 'hello world', 5, 6, 7, 8]
+# a[0:len(a):1]
+# print(a)
+# map_ = {
+#     'first_name': 'shivank',
+#     'last_name': ' gautam',
+#     'campany': 'regex'
+# }
+# print (map_['first_name'])
+# print (map_['campany'])
+# print (map_.get('comapany', 'not found'))
+# print (map_.pop('comapany', 'not found'))
+# lst = [1,2,3,4,5,6,7,8,9,10]
+i=0
+while i < 5:
+    print(1)
+    i +=1 # i = i + 1
+    
+
+
+
+
+
+
